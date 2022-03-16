5d11882c203bc1e51b3d537e42e4b2de8ab17023
Vanim77 <vanimouzad123@gmail.com>
2022-03-16 15:40:39 -0300

    impl: Matchs migration

---
+++ app/backend/src/database/migrations/20220316181152-create-matchs.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Matchs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER
+      },
+      inProgress: {
+        type: Sequelize.TINYINT
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Matchs');
+  }
+};

7c9cb79b5bcb879c280afcfb893405d265303296
Vanim77 <vanimouzad123@gmail.com>
2022-03-16 15:40:29 -0300

    impl: Clubs migration

---
+++ app/backend/src/database/migrations/20220316181217-create-clubs.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Clubs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      clubName: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Clubs');
+  }
+};

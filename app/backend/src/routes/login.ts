3c6ef6d913690c2f659ffd13833c55ad80009ba5
Vanim77 <vanimouzad123@gmail.com>
2022-03-18 18:48:53 -0300

    impl: GET route /login/validate

--- app/backend/src/routes/login.ts
+++ app/backend/src/routes/login.ts
@@ -1,8 +1,22 @@
 import { Router } from 'express';
+import emailValidation from '../middlewares/emailValidation';
+import passwordValidation from '../middlewares/passwordValidation';
 import LoginController from '../controllers/loginController';
+import tokenValidation from '../middlewares/tokenValidation';
 
 const loginRoutes = Router();
 
-loginRoutes.post('/login', LoginController);
+loginRoutes.post(
+  '/login',
+  emailValidation,
+  passwordValidation,
+  LoginController.login,
+);
+
+loginRoutes.get(
+  '/login/validate',
+  tokenValidation,
+  LoginController.loginValidate,
+);
 
 export default loginRoutes;

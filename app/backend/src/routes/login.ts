64429cf58fe4d0f8f01b1a0152b7e8882254eb65
Vanim77 <vanimouzad123@gmail.com>
2022-03-18 13:09:06 -0300

    impl: POST route /login

--- app/backend/src/routes/login.ts
+++ app/backend/src/routes/login.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import LoginController from '../controllers/loginController';
 
 const loginRoutes = Router();
+
+loginRoutes.post('/login', LoginController);
 
 export default loginRoutes;

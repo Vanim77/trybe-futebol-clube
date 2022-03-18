8fe7f79dfe4601fc137d733fe18aa4fe6c6758f8
Vanim77 <vanimouzad123@gmail.com>
2022-03-18 18:49:30 -0300

    impl: loginValidate on Controller

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -2,10 +2,10 @@ import { Request, Response } from 'express';
 import StatusCode from '../utils/StatusCode';
 import LoginService from '../services/loginService';
 
-const LoginController = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  const response = await LoginService({ email, password });
+  const response = await LoginService.login({ email, password });
 
   if (!response.user) {
     return res.status(StatusCode.Unauthorized).json(response);
@@ -14,4 +14,19 @@ const LoginController = async (req: Request, res: Response) => {
   res.status(StatusCode.OK).json(response);
 };
 
-export default LoginController;
+const loginValidate = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  const response = await LoginService.checkLoginRole(email);
+
+  if (!response.role) {
+    return res.status(StatusCode.Unauthorized).json(response);
+  }
+
+  return res.status(StatusCode.OK).json(response.role);
+};
+
+export default {
+  login,
+  loginValidate,
+};

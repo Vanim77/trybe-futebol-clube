c06442afd4775ca0695b478782af781b93b86421
Vanim77 <vanimouzad123@gmail.com>
2022-03-21 18:57:37 -0300

    add: @types bcrypt to dependencies

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -13,19 +13,9 @@ const { expect } = chai;
 
 let ChaiHttpResponse: Response;
 
-const loginSucess = {
-  user: {
-    id: 77,
-    username: 'vanim77',
-    role: 'atacante',
-    email: 'vanim77@gmail.com'
-  },
-  token: '@abcdefgh0987654321@'
-}
-
 const token = '@abcdefgh0987654321@';
 
-describe('Testa a rota POST /login', () => {
+describe('Testa a rota POST /login em caso de sucesso', () => {
   before(async () => {
     sinon.stub(Users, 'findOne')
       .resolves({
@@ -36,6 +26,9 @@ describe('Testa a rota POST /login', () => {
         password: '123456789'
       } as Users)
 
+    sinon.stub(bcrypt, 'compareSync')
+      .resolves(true);
+
     sinon.stub(jwt, 'sign')
       .resolves(token)
   })
@@ -43,6 +36,7 @@ describe('Testa a rota POST /login', () => {
   after(() => {
     (Users.findOne as sinon.SinonStub).restore();
     (jwt.sign as sinon.SinonStub).restore();
+    (bcrypt.compareSync as sinon.SinonStub).restore();
   })
 
   it(`Deve retornar um objeto com as propriedades esperadas
@@ -60,6 +54,49 @@ describe('Testa a rota POST /login', () => {
     expect(ChaiHttpResponse.body).to.have.a.property('token');
   })
 })
+
+describe('Testa as validações da rota POST /login em caso de erro', () => {
+  it('Deveria quebrar caso não passe um email', async () => {
+    ChaiHttpResponse = await chai.request(app)
+    .post('/login')
+    .send({ password: '123456789' })
+
+    expect(ChaiHttpResponse).to.have.status(401);
+    expect(ChaiHttpResponse.body).to.have.a.property('message');
+    expect(ChaiHttpResponse.body.message).to.be.eq('All fields must be filled');
+  })
+
+  it('Deveria quebrar caso não passe um email no formato correto', async () => {
+    ChaiHttpResponse = await chai.request(app)
+    .post('/login')
+    .send({ email: 'testetestegmail.com', password: '123456789' })
+
+    expect(ChaiHttpResponse).to.have.status(401);
+    expect(ChaiHttpResponse.body).to.have.a.property('message');
+    expect(ChaiHttpResponse.body.message).to.be.eq('Incorrect email or password');
+  })
+
+  it('Deveria quebrar caso não passe um password', async () => {
+    ChaiHttpResponse = await chai.request(app)
+    .post('/login')
+    .send({ email: 'teste@gmail.com' })
+
+    expect(ChaiHttpResponse).to.have.status(401);
+    expect(ChaiHttpResponse.body).to.have.a.property('message');
+    expect(ChaiHttpResponse.body.message).to.be.eq('All fields must be filled');
+  })
+
+  it('Deveria quebrar caso não passe um password no formato correto', async () => {
+    // O password tem um tamanho mínimo de 6 caracteres, portanto '777' está no formato incorreto
+    ChaiHttpResponse = await chai.request(app)
+    .post('/login')
+    .send({ email: 'teste@gmail.com', password: '777' })
+
+    expect(ChaiHttpResponse).to.have.status(401);
+    expect(ChaiHttpResponse.body).to.have.a.property('message');
+    expect(ChaiHttpResponse.body.message).to.be.eq('Incorrect email or password');
+  })
+});
 
 // describe('Testa a rota GET /login/validate', () => {
 //   chai.request(app).get('/login/validate')

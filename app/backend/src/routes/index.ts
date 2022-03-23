f3eb4d2af5737053ed120a6895d1dd778f7a2aa7
Vanim77 <vanimouzad123@gmail.com>
2022-03-23 15:19:28 -0300

    impl: route /clubs on index

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import clubsRouter from './clubs';
 import loginRoutes from './login';
 
 const router = Router();
 
 router.use(loginRoutes);
+router.use(clubsRouter);
 
 export default router;

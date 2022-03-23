177984e124738b3e207d8d499b27c88ae6d9c011
Vanim77 <vanimouzad123@gmail.com>
2022-03-23 16:45:15 -0300

    impl: getById on ClubsController

--- app/backend/src/controllers/clubsController.ts
+++ app/backend/src/controllers/clubsController.ts
@@ -1,12 +1,26 @@
 import { Request, Response } from 'express';
+import StatusCode from '../utils/StatusCode';
 import ClubsService from '../services/clubsService';
 
 const getAll = async (_req: Request, res: Response) => {
   const response = await ClubsService.getAll();
 
-  return res.status(200).json(response);
+  return res.status(StatusCode.OK).json(response);
+};
+
+const getById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const response = await ClubsService.getById(Number(id));
+
+  if (!response.clubName) {
+    return res.status(StatusCode.BadRequest).json(response);
+  }
+
+  return res.status(StatusCode.OK).json(response);
 };
 
 export default {
   getAll,
+  getById,
 };

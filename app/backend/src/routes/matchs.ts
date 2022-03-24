a8df38cf9e73824ab43fee988ebfc8826087f52e
Vanim77 <vanimouzad123@gmail.com>
2022-03-24 20:11:15 -0300

    impl: POST route and PATCH route /matchs

--- app/backend/src/routes/matchs.ts
+++ app/backend/src/routes/matchs.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import tokenValidation from '../middlewares/tokenValidation';
 import MatchsController from '../controllers/matchsController';
 
 const matchsRoutes = Router();
 
 matchsRoutes.get('/matchs', MatchsController.getAll);
+matchsRoutes.post('/matchs', tokenValidation, MatchsController.createInProgressMatch);
+matchsRoutes.patch('/matchs/:id/finish', MatchsController.updateMatch);
 
 export default matchsRoutes;

993ccf80c2e47663293dcd88b4a343ca0775a6ce
Vanim77 <vanimouzad123@gmail.com>
2022-03-24 17:01:43 -0300

    impl: mock allMatchs

--- app/backend/src/tests/mocks.ts
+++ app/backend/src/tests/mocks.ts
@@ -5,3 +5,48 @@ export const allClubs = [
   { id: 4, clubName: 'Corinthians' },
   { id: 5, clubName: 'Cruzeiro' },
 ];
+
+export const allMatchs = [
+  {
+    "id": 1,
+    "homeTeam": 16,
+    "homeTeamGoals": 1,
+    "awayTeam": 8,
+    "awayTeamGoals": 1,
+    "inProgress": 1,
+    "homeClub": {
+      "clubName": "São Paulo"
+    },
+    "awayClub": {
+      "clubName": "Grêmio"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeam": 9,
+    "homeTeamGoals": 1,
+    "awayTeam": 14,
+    "awayTeamGoals": 1,
+    "inProgress": 0,
+    "homeClub": {
+      "clubName": "Internacional"
+    },
+    "awayClub": {
+      "clubName": "Santos"
+    }
+  },
+  {
+    "id": 3,
+    "homeTeam": 4,
+    "homeTeamGoals": 3,
+    "awayTeam": 11,
+    "awayTeamGoals": 0,
+    "inProgress": 0,
+    "homeClub": {
+      "clubName": "Corinthians"
+    },
+    "awayClub": {
+      "clubName": "Napoli-SC"
+    }
+  }
+];

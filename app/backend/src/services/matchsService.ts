f0d107c9d5743336b4de67f23cd2225a6f2dc30c
Vanim77 <vanimouzad123@gmail.com>
2022-03-24 20:12:36 -0300

    impl: create/update matchs on MatchsService

--- app/backend/src/services/matchsService.ts
+++ app/backend/src/services/matchsService.ts
@@ -1,3 +1,4 @@
+import checkTeamsExists from '../utils/checkTeamsExists';
 import Clubs from '../database/models/Clubs';
 import Matchs from '../database/models/Matchs';
 
@@ -28,7 +29,37 @@ const getByQuery = async (inProgress: string | undefined) => {
   return matchs;
 };
 
+const create = async (newMatch: Matchs) => {
+  if (newMatch.homeTeam === newMatch.awayTeam) {
+    return { message: 'It is not possible to create a match with two equal teams' };
+  }
+
+  const homeTeam = await checkTeamsExists(newMatch.homeTeam);
+  const awayTeam = await checkTeamsExists(newMatch.awayTeam);
+
+  if (!homeTeam || !awayTeam) {
+    return { message: 'There is no team with such id!' };
+  }
+
+  const createdMatch = await Matchs.create(newMatch);
+
+  return {
+    id: createdMatch.id,
+    homeTeam: createdMatch.homeTeam,
+    homeTeamGoals: createdMatch.homeTeamGoals,
+    awayTeam: createdMatch.awayTeam,
+    awayTeamGoals: createdMatch.awayTeamGoals,
+    inProgress: createdMatch.inProgress,
+  };
+};
+
+const updateMatch = async (id: number) => {
+  await Matchs.update({ inProgress: 0 }, { where: { id } });
+};
+
 export default {
   getAll,
   getByQuery,
+  create,
+  updateMatch,
 };

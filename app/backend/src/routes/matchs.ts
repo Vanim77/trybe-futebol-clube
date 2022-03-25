10f41a1dd3f36c9844ebb940b5309d76afe80a54
Vanim77 <vanimouzad123@gmail.com>
2022-03-25 14:42:50 -0300

    impl: PATCH route /matchs/:id

--- app/backend/src/routes/matchs.ts
+++ app/backend/src/routes/matchs.ts
@@ -6,6 +6,7 @@ const matchsRoutes = Router();
 
 matchsRoutes.get('/matchs', MatchsController.getAll);
 matchsRoutes.post('/matchs', tokenValidation, MatchsController.createInProgressMatch);
+matchsRoutes.patch('/matchs/:id', MatchsController.updateGoals);
 matchsRoutes.patch('/matchs/:id/finish', MatchsController.updateMatch);
 
 export default matchsRoutes;

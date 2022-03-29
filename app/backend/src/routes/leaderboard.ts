daf747177d4e0ddce110ba6712b1cd9d929e4227
Vanim77 <vanimouzad123@gmail.com>
2022-03-29 00:46:15 -0300

    impl: GET route /leaderboard/away

--- app/backend/src/routes/leaderboard.ts
+++ app/backend/src/routes/leaderboard.ts
@@ -5,5 +5,6 @@ const leaderboardRouter = Router();
 
 leaderboardRouter.get('/leaderboard', LeaderboardController.getLeaderboard);
 leaderboardRouter.get('/leaderboard/home', LeaderboardController.getHomeLeaderboard);
+leaderboardRouter.get('/leaderboard/away', LeaderboardController.getAwayLeaderboard);
 
 export default leaderboardRouter;
